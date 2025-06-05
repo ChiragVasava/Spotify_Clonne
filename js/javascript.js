14ae3dc96ff2bc5e0204962d9e727f229811dc57
ChiragVasava <149458923+ChiragVasava@users.noreply.github.com>
2025-06-06 02:03:13 +0530

    Added Function To Play Song
    In the main() function we first get the list of all songs using the getSongs() function which is defined in this file. The getSongs() function gets the list from an external server. Then, we play the first song in the list using the Audio() object from the JavaScript API.

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,32 @@
 console.log("Lets Write Javascript");
+
+async function getSongs() {
+
+    let a = await fetch("http://192.168.29.83:3000/songs/English/")
+    let response = await a.text();
+    console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href)
+        }
+    }
+    return songs
+
+
+}
+async function main() {
+    //Get the list of all the songs
+    let songs = await getSongs()
+    console.log(songs)
+
+    //Play The First Song
+    var audio = new Audio(songs[0]);
+    audio.play();
+
+}
+main()
